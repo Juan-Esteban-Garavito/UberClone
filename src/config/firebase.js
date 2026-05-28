8f9a88907e6d748800fe2a9c485163f17bd3788b
Juan-Esteban-Garavito <juan.garavito@correo.tdea.edu.co>
2026-05-28 02:19:18 -0500

    chore: connect Firebase config with real project credentials

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,12 +1,15 @@
 import firebase from '@react-native-firebase/app';
 
+// Firebase project: uberclone-de706
+// This config mirrors the native google-services.json
+// The actual SDK connection is handled natively by @react-native-firebase
 const firebaseConfig = {
-  apiKey: 'TU_API_KEY',
-  authDomain: 'TU_DOMINIO.firebaseapp.com',
-  projectId: 'TU_PROJECT_ID',
-  storageBucket: 'TU_BUCKET.appspot.com',
-  messagingSenderId: 'TU_SENDER_ID',
-  appId: 'TU_APP_ID',
+  apiKey:            'AIzaSyAS5WYRBZ3kAilYdTg2fUJNhpfZmUYVNKM',
+  authDomain:        'uberclone-de706.firebaseapp.com',
+  projectId:         'uberclone-de706',
+  storageBucket:     'uberclone-de706.firebasestorage.app',
+  messagingSenderId: '1098627871715',
+  appId:             '1:1098627871715:android:0592cbe6607ad0be1026d6',
 };
 
 export default firebaseConfig;
